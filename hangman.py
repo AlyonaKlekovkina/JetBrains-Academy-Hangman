805141f25b5b204a02378422396711a0c7f66fa3
AlyonaKlekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2020-09-10 11:18:23 +0300

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -3,31 +3,52 @@ import random
 
 
 def check_letter():
-    global n, set_to_print
+    global n, set_to_print, letter
     letter = input("Input a letter: ")
-    if letter in the_word:
-        if letter in guessed_letters:
-            print("No improvements")
-            n -= 1
+    if check_input():
+        if letter in the_word:
+            if letter in guessed_letters:
+                print("No improvements")
+                n -= 1
+                if n == 0:
+                    print("You are hanged!")
+            else:
+                guessed_letters.add(letter)
+                set_to_print = set(the_word)
+                if len(set_to_print) == len(guessed_letters):
+                    print("You guessed the word {}!\nYou survived!".format(the_word))
+                    exit()
+                else:
+                    return to_print
+        else:
+            if letter in all_inputted_letters:
+                print("You already typed this letter")
+                n -= 1
+            else:
+                print("No such letter in the word")
+                all_inputted_letters.add(letter)
+                n -= 1
+
             if n == 0:
                 print("You are hanged!")
-        else:
-            guessed_letters.add(letter)
-            set_to_print = set(the_word)
-            if len(set_to_print) == len(guessed_letters):
-                print("You guessed the word!\nYou survived!")
-            else:
-                return to_print
-    else:
-        print("No such letter in the word")
-        n -= 1
-        if n == 0:
-            print("You are hanged!")
+
+
+def check_input():
+    global letter
+    if len(letter) != 1 or letter == '':
+        print("You should input a single letter")
+        return False
+    if not letter.islower():
+        print("It is not and ASCII lowercase letter")
+        return False
+    return True
+
 
 print("H A N G M A N")
 the_list = ['python', 'java', 'kotlin', 'javascript']
 the_word = random.choice(the_list)
 guessed_letters = set()
+all_inputted_letters = set()
 n = 8
 while n > 0:
     to_print = ''
