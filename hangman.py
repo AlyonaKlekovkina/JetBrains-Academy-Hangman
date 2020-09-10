096fcbc510d1391454f6dcc7685471ea70e9faa1
AlyonaKlekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2020-09-10 10:00:18 +0300

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,14 +1,33 @@
 import random
 # Write your code here
+
+
+def check_letter():
+    letter = input("Input a letter: ")
+    if letter in the_word:
+        guessed_letters.add(letter)
+        return to_print
+    else:
+        print("No such letter in the word")
+
+
 print("H A N G M A N")
 the_list = ['python', 'java', 'kotlin', 'javascript']
 the_word = random.choice(the_list)
-first_letters = the_word[:3]
-last_letters = the_word[3:]
-replacement = first_letters + len(last_letters) * '-'
+guessed_letters = set()
+n = 8
+while n > 0:
+    to_print = ''
 
-the_guess = input("Guess the word {}: ".format(replacement))
-if the_word == the_guess:
-    print("You survived!")
-else:
-    print("You are hanged!")
+    for i in the_word:
+        if i in guessed_letters:
+            to_print += i
+        else:
+            to_print += '-'
+
+    print("\n", to_print, sep="")
+
+    check = check_letter()
+    n -= 1
+    if n == 0:
+        print("\nThanks for playing!\nWe'll see how well you did in the next stage")
